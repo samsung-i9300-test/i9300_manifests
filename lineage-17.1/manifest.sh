da24eda56c2c94aa0ba3831e01353571731b88ea
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-08 11:23:08 +0800

    i9300: components
    https://github.com/CustomROMs/android_local_manifests_i9300/blob/lineage-17.1/local_manifest.xml

---
+++ lineage-17.1/manifest.sh
@@ -0,0 +1,11 @@
+git clone -b lineage-17.1-ksu https://github.com/samsung-i9300-test/chrono_kernel_samsung_smdk4412 kernel/samsung/smdk4412 --depth=1
+
+git clone -b lineage-17.1 https://github.com/samsung-i9300-test/android_device_samsung_i9300 device/samsung/i9300
+
+git clone -b lineage-17.1 https://github.com/samsung-i9300-test/android_vendor_samsung vendor/samsung
+
+git clone -b lineage-17.1 https://github.com/samsung-i9300-test/android_device_samsung_smdk4412-common device/samsung/smdk4412-common 
+
+git clone -b lineage-17.1 https://github.com/samsung-i9300-test/android_device_samsung_smdk4412-qcom-common device/samsung/smdk4412-qcom-common
+
+git clone -b lineage-17.1 https://github.com/samsung-i9300-test/android_hardware_samsung hardware/samsung
